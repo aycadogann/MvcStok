5526537e418e099d1558a432e93bc8222b6427ab
Ayca Dogan <dogan.aycaa@gmail.com>
2025-06-23 17:20:31 +0300

    UrunListele bolumunde duzenlemeler yapildi ve YeniUrunEkle bolumunde dropdownlist duzenlendi

--- MvcStok/Controllers/UrunController.cs
+++ MvcStok/Controllers/UrunController.cs
@@ -21,15 +21,27 @@ namespace MvcStok.Controllers
         [HttpGet]
         public ActionResult YeniUrunEkle()
         {
+            List<SelectListItem> degerler = (from i in db.Kategoriler.ToList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = i.KategoriAd,
+                                                   Value = i.KategoriId.ToString()
+                                               }).ToList();
+            ViewBag.dgr = degerler;
             return View();
         }
 
         [HttpPost]
         public ActionResult YeniUrunEkle(Urunler urun)
         {
+            var ktgr = db.Kategoriler.Where(k => k.KategoriId == urun.Kategoriler.KategoriId).FirstOrDefault();
+            urun.Kategoriler = ktgr;
             db.Urunler.Add(urun);
             db.SaveChanges();
-            return View();
+            return RedirectToAction("UrunListele");
         }
+
+        
+
     }
 }
